2a6ec6292dd8c5cae80f521e15ba4dc6c19c762b
mwangiowen <owen31588@gmail.com>
2024-01-21 10:00:23 +0300

    Done

--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -7,60 +7,58 @@ from flask import request
 from app import app
 from models import db, Bakery, BakedGood
 
+
 class TestApp:
-    '''Flask application in flask_app.py'''
+    """Flask application in flask_app.py"""
 
     def test_creates_baked_goods(self):
-        '''can POST new baked goods through "/baked_goods" route.'''
+        """can POST new baked goods through "/baked_goods" route."""
 
         with app.app_context():
-
             af = BakedGood.query.filter_by(name="Apple Fritter").first()
             if af:
                 db.session.delete(af)
                 db.session.commit()
 
             response = app.test_client().post(
-                '/baked_goods',
+                "/baked_goods",
                 data={
                     "name": "Apple Fritter",
                     "price": 2,
                     "bakery_id": 5,
-                }
+                },
             )
 
             af = BakedGood.query.filter_by(name="Apple Fritter").first()
 
             assert response.status_code == 201
-            assert response.content_type == 'application/json'
+            assert response.content_type == "application/json"
             assert af.id
 
     def test_updates_bakeries(self):
-        '''can PATCH bakeries through "bakeries/<int:id>" route.'''
+        """can PATCH bakeries through "bakeries/<int:id>" route."""
 
         with app.app_context():
-
             mb = Bakery.query.filter_by(id=1).first()
             mb.name = "ABC Bakery"
             db.session.add(mb)
             db.session.commit()
 
             response = app.test_client().patch(
-                '/bakeries/1',
-                data = {
+                "/bakeries/1",
+                data={
                     "name": "Your Bakery",
-                }
+                },
             )
 
-            assert(response.status_code == 200)
-            assert(response.content_type == 'application/json')
-            assert(mb.name == "Your Bakery")
+            assert response.status_code == 200
+            assert response.content_type == "application/json"
+            assert mb.name == "Your Bakery"
 
     def test_deletes_baked_goods(self):
-        '''can DELETE baked goods through "baked_goods/<int:id>" route.'''
+        """can DELETE baked goods through "baked_goods/<int:id>" route."""
 
         with app.app_context():
-            
             af = BakedGood.query.filter_by(name="Apple Fritter").first()
             if not af:
                 af = BakedGood(
@@ -70,12 +68,9 @@ class TestApp:
                 )
                 db.session.add(af)
                 db.session.commit()
-            
 
-            response = app.test_client().delete(
-                f'/baked_goods/{af.id}'
-            )
+            response = app.test_client().delete(f"/baked_goods/{af.id}")
 
-            assert(response.status_code == 200)
-            assert(response.content_type == 'application/json')
-            assert(not BakedGood.query.filter_by(name="Apple Fritter").first())
+            assert response.status_code == 204
+            assert response.content_type == "application/json"
+            assert not BakedGood.query.filter_by(name="Apple Fritter").first()
